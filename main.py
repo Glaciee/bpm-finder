2d39af90ac7fe7f075ddca1a98e56a1af6bd7f1f
Samuel Zhang <120329205+Glaciee@users.noreply.github.com>
2023-11-18 18:28:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from time import *
 
+#if click:
+time_btwn_clicks = []
+total_clicks = len(time_btwn_clicks) #total clicks is the amount of values inside time_btwn_clicks
+total_time = 0
+for i in range (total_clicks):
+  total_time = total_time + time_btwn_clicks[i]
+avg_time = 0
+#for every click
+  #time_btwn_clicks.append(seconds between clicks, which is equal to current total time - previous total time)
+
+#for i in range (total_clicks)
+  #total_time = total_time + time_btwn_clicks[i]
+
+#avg_time = 
