45a9e76f187ad450c339c4908ab9c7406a0b70dd
Dogukan Icer <151408803+ackee39@users.noreply.github.com>
2023-11-18 23:40:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let numberOfProcess = 0;
+let seconds = 0; 
+let tens = 0;
+let Interval ;
 
 const num = document.querySelector('.num');
-let seconds = 0; 
-let tens = 0; 
-  var buttonStart = document.getElementById('button-start');
-  var buttonStop = document.getElementById('button-stop');
-  var buttonReset = document.getElementById('button-reset');
-  var Interval ;
+const buttonStart = document.getElementById('button-start');
+const buttonStop = document.getElementById('button-stop');
+const buttonReset = document.getElementById('button-reset');
 const appendTens = document.getElementById("tens");
 const appendSeconds = document.getElementById("seconds");
 
@@ -15,41 +15,48 @@ const clickHandler = () => {
   num.innerHTML = numberOfProcess;
 };
 
-buttonStart.addEventListener('click', function() {
-      numberOfProcess++;
+buttonStart.addEventListener('click',() => {
+	numberOfProcess++;
   num.innerHTML = numberOfProcess;
-    clearInterval(Interval);
-     Interval = setInterval(startTimer, 10);
-  });
+  clearInterval(Interval);
+  Interval = setInterval(startTimer, 10);
+});
   
-buttonStop.addEventListener('click', function() {
+buttonStop.addEventListener('click',() => {
   clearInterval(Interval);
 });
- 
-  function startTimer () {
-    tens++; 
-    
-    if(tens <= 9){
-      appendTens.innerHTML = "0" + tens;
-    }
-    
-    if (tens > 9){
-      appendTens.innerHTML = tens;
-      
-    } 
-    
-    if (tens > 99) {
-      console.log("seconds");
-      seconds++;
-      appendSeconds.innerHTML = "0" + seconds;
-      tens = 0;
-      appendTens.innerHTML = "0" + 0;
-    }
-    
-    if (seconds > 9){
-      appendSeconds.innerHTML = seconds;
-    }
+
+buttonReset.onclick = () => {
+	clearInterval(Interval);
+  tens = "00";
+  seconds = "00";
+  appendTens.innerHTML = tens;
+  appendSeconds.innerHTML = seconds;
+  num.innerHTML = 0;
+  numberOfProcess = 0;
+}
   
+function startTimer () {
+	tens++; 
+    
+  if(tens <= 9){
+  	appendTens.innerHTML = "0" + tens;
+  }
+    
+  if (tens > 9){
+  	appendTens.innerHTML = tens;
+  } 
+    
+  if (tens > 99) {
+  	console.log("seconds");
+    seconds++;
+    appendSeconds.innerHTML = "0" + seconds;
+    tens = 0;
+    appendTens.innerHTML = "0" + 0;
+  }
+    
+  if (seconds > 9){
+  	appendSeconds.innerHTML = seconds;
   }
   
-add.addEventListener('click', clickHandler);
+}
